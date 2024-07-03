e70519d84c163641931fbc4c392136218d614d15
MOHD NEHAL KHAN <nehalk482@gmail.com>
2024-07-03 12:00:50 +0530

    Program 2 uploaded

---
+++ Program2.sh
@@ -0,0 +1,17 @@
+#Program 2 - Write a shell script to compare two numbers. The numbers are to be read from the user. The operations to be performed are equal, not equal, greater than, less than, greater than or equal to, less than or equal to.
+
+a = 12
+b = 10
+
+if [$a -eq $b] 
+then
+echo "[$a -eq $b]:The condition passed as a is equal to b"
+else
+echo "[$a -eq $b]:The condition passed as a is not equal to b"
+fi
+if [$a -ne $b]
+then
+echo "[$a -ne $b]:The condition passed as a is not equal to b"
+else
+echo "[$a -ne $b]:The condition passed as a is equal to b"
+fi
