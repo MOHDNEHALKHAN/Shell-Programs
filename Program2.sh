ed602950e8ab06c1fd9c15e686b0ef04052a2ac5
MOHD NEHAL KHAN <nehalk482@gmail.com>
2024-07-03 23:07:21 +0530

    Fixed prev prog & new prog added

--- Program2.sh
+++ Program2.sh
@@ -1,15 +1,15 @@
 #Program 2 - Write a shell script to compare two numbers. The numbers are to be read from the user. The operations to be performed are equal, not equal, greater than, less than, greater than or equal to, less than or equal to.
 
-a = 12
-b = 10
+a=12
+b=12
 
-if [$a -eq $b] 
+if [ "$a" -eq "$b" ] 
 then
 echo "[$a -eq $b]:The condition passed as a is equal to b"
 else
 echo "[$a -eq $b]:The condition passed as a is not equal to b"
 fi
-if [$a -ne $b]
+if [ "$a" -ne "$b" ]
 then
 echo "[$a -ne $b]:The condition passed as a is not equal to b"
 else
---
+++ Program3.sh
@@ -0,0 +1,9 @@
+#Program3 - Write a shell script to swap two numbers.
+
+first=5
+second=10
+temp=$first
+first=$second
+second=$temp
+echo "After swapping, no's are :"
+echo "first = $first , second = $second"
