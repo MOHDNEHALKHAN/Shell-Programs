1f03f4a8b730e8c95f422356f19e6b5333119a74
MOHD NEHAL KHAN <nehalk482@gmail.com>
2024-07-04 08:55:26 +0530

    Prog added to compare two no.s

---
+++ Program4.sh
@@ -0,0 +1,10 @@
+#Program4 - Write a shell script to compare two numbers and print whether they are equal or not.
+
+a=30
+b=20
+if [ "$a" == "$b" ]
+then
+echo "a is equal to b"
+else
+echo "a is not equal to b"
+fi
