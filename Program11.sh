8e9cdf4193ba14f0cd6ec585ff471c9b37523d07
MOHD NEHAL KHAN <nehalk482@gmail.com>
2024-07-10 19:43:42 +0530

    Program11 added

---
+++ Program11.sh
@@ -0,0 +1,14 @@
+#Program to print numbers from 1 to 9 using for loop
+
+for num in 1 2 3 4 5 6 7 8 9
+do
+echo "Number : $num"
+done
+
+#or
+
+for i in 1 2 3 4 5
+do
+sum=$(expr $sum + $i)
+done
+echo $sum
