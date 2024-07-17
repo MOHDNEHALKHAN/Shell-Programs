e6a36c9b1c02e60bfbcafe0a703678e5467fcec6
MOHD NEHAL KHAN <nehalk482@gmail.com>
2024-07-17 16:57:11 +0530

    Prog 17 added

---
+++ Program17.sh
@@ -0,0 +1,9 @@
+#Program17 : To print the string in the terminal
+
+echo "world"
+echo "beautiful"
+
+echo -n  "world"
+echo "beautiful"
+echo "hello world & otherthings"
+echo -e "hello world \n & otherthings"
