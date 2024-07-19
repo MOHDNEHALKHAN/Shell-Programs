83c968a8fea0b533f0ad4a257d66922ca94df604
MOHD NEHAL KHAN <nehalk482@gmail.com>
2024-07-19 15:20:05 +0530

    Program of string concatination

---
+++ Program19.sh
@@ -0,0 +1,13 @@
+#Script to Concatenate Strings
+
+# Declaration of first String 
+str1="Welcome"
+
+# Declaration of Second String
+str2=" to Manipal University Rajasthan."
+
+# Combining first and second string 
+str3="$str1$str2"
+
+# print the concatenated string
+echo $str3
